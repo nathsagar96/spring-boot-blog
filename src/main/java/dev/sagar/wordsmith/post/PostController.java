08c517700e967e4f95fd0ea6708e4380a9fd8be8
nathsagar96 <nathsagar96@gmail.com>
2024-10-03 23:11:07 +0530

    use constants for pagination and sorting

--- src/main/java/dev/sagar/wordsmith/post/PostController.java
+++ src/main/java/dev/sagar/wordsmith/post/PostController.java
@@ -14,19 +14,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import static dev.sagar.wordsmith.post.PageConstants.PAGE_NUMBER;
+import static dev.sagar.wordsmith.post.PageConstants.PAGE_SIZE;
+import static dev.sagar.wordsmith.post.PageConstants.SORT_BY;
+import static dev.sagar.wordsmith.post.PageConstants.SORT_DIRECTION;
+
 @RestController
 @RequestMapping("/api/posts")
 @RequiredArgsConstructor
 public class PostController {
 
+
     private final PostService postService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public PagedResponse<PostResponseDto> getAllPosts(@RequestParam(defaultValue = "1") final Integer page,
-                                                      @RequestParam(defaultValue = "10") final Integer size,
-                                                      @RequestParam(defaultValue = "createdAt") final String sortBy,
-                                                      @RequestParam(defaultValue = "desc") final String direction) {
+    public PagedResponse<PostResponseDto> getAllPosts(@RequestParam(defaultValue = PAGE_NUMBER) final Integer page,
+                                                      @RequestParam(defaultValue = PAGE_SIZE) final Integer size,
+                                                      @RequestParam(defaultValue = SORT_BY) final String sortBy,
+                                                      @RequestParam(defaultValue = SORT_DIRECTION) final String direction) {
         return postService.getAllPosts(page, size, sortBy, direction);
     }
 
@@ -58,20 +64,20 @@ public class PostController {
     @GetMapping("/category/{categoryId}")
     @ResponseStatus(HttpStatus.OK)
     public PagedResponse<PostResponseDto> getPostsByCategoryId(@PathVariable final Integer categoryId,
-                                                               @RequestParam(defaultValue = "1") final Integer page,
-                                                               @RequestParam(defaultValue = "10") final Integer size,
-                                                               @RequestParam(defaultValue = "createdAt") final String sortBy,
-                                                               @RequestParam(defaultValue = "desc") final String direction) {
+                                                               @RequestParam(defaultValue = PAGE_NUMBER) final Integer page,
+                                                               @RequestParam(defaultValue = PAGE_SIZE) final Integer size,
+                                                               @RequestParam(defaultValue = SORT_BY) final String sortBy,
+                                                               @RequestParam(defaultValue = SORT_DIRECTION) final String direction) {
         return postService.getPostsByCategoryId(categoryId, page, size, sortBy, direction);
     }
 
     @GetMapping("/user/{userId}")
     @ResponseStatus(HttpStatus.OK)
     public PagedResponse<PostResponseDto> getPostsByUserId(@PathVariable final Long userId,
-                                                           @RequestParam(defaultValue = "1") final Integer page,
-                                                           @RequestParam(defaultValue = "10") final Integer size,
-                                                           @RequestParam(defaultValue = "createdAt") final String sortBy,
-                                                           @RequestParam(defaultValue = "desc") final String direction) {
+                                                           @RequestParam(defaultValue = PAGE_NUMBER) final Integer page,
+                                                           @RequestParam(defaultValue = PAGE_SIZE) final Integer size,
+                                                           @RequestParam(defaultValue = SORT_BY) final String sortBy,
+                                                           @RequestParam(defaultValue = SORT_DIRECTION) final String direction) {
         return postService.getPostsByUserId(userId, page, size, sortBy, direction);
     }
 
@@ -79,20 +85,20 @@ public class PostController {
     @ResponseStatus(HttpStatus.OK)
     public PagedResponse<PostResponseDto> getPostsByUserIdAndCategoryId(@PathVariable final Long userId,
                                                                         @PathVariable final Integer categoryId,
-                                                                        @RequestParam(defaultValue = "1") final Integer page,
-                                                                        @RequestParam(defaultValue = "10") final Integer size,
-                                                                        @RequestParam(defaultValue = "createdAt") final String sortBy,
-                                                                        @RequestParam(defaultValue = "desc") final String direction) {
+                                                                        @RequestParam(defaultValue = PAGE_NUMBER) final Integer page,
+                                                                        @RequestParam(defaultValue = PAGE_SIZE) final Integer size,
+                                                                        @RequestParam(defaultValue = SORT_BY) final String sortBy,
+                                                                        @RequestParam(defaultValue = SORT_DIRECTION) final String direction) {
         return postService.getPostsByUserIdAndCategoryId(userId, categoryId, page, size, sortBy, direction);
     }
 
     @GetMapping("/search/{searchTerm}")
     @ResponseStatus(HttpStatus.OK)
     public PagedResponse<PostResponseDto> getPostsBySearchTerm(@PathVariable final String searchTerm,
-                                                               @RequestParam(defaultValue = "1") final Integer page,
-                                                               @RequestParam(defaultValue = "10") final Integer size,
-                                                               @RequestParam(defaultValue = "createdAt") final String sortBy,
-                                                               @RequestParam(defaultValue = "desc") final String direction) {
+                                                               @RequestParam(defaultValue = PAGE_NUMBER) final Integer page,
+                                                               @RequestParam(defaultValue = PAGE_SIZE) final Integer size,
+                                                               @RequestParam(defaultValue = SORT_BY) final String sortBy,
+                                                               @RequestParam(defaultValue = SORT_DIRECTION) final String direction) {
         return postService.getByTitleContaining(searchTerm, page, size, sortBy, direction);
     }
 }
